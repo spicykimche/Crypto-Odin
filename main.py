881240aba56ccff58dcab8a4229039f3e7636ca7
Junho Kim <jnhkm311@gmail.com>
2021-12-04 18:53:46 -0800

    Add market dominance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dependency.alternativeme import get_crypto_tickers, get_global_market_info, get_fear_and_greed_index, filter_biggest_marketcaps, filter_by_percentage_change_24h, filter_by_percentage_change_7d
 from dependency.news_curator import get_crypto_news
-from util.formatter import print_disclaimer, print_state_of_the_crypto, print_crypto_news, print_tickers
+from util.formatter import print_disclaimer, print_state_of_the_crypto, print_crypto_news, print_tickers, print_market_dominance
 
 if __name__ == "__main__":
     tickers = get_crypto_tickers()
@@ -9,6 +9,7 @@ if __name__ == "__main__":
 
     print_disclaimer()
     print_state_of_the_crypto(market_info, fng)
+    print_market_dominance(filter_biggest_marketcaps(tickers), market_info)
     print_crypto_news(get_crypto_news())
     print_tickers("BIG MARKET CAPS", filter_biggest_marketcaps(tickers), "percentage_change_24h")
     print_tickers("WEEKLY MARKET MOVERS", filter_by_percentage_change_7d(tickers, True, 5), "percentage_change_7d")
